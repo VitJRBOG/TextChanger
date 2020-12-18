2c286b01075d78f1c35cea7bc13fc97d69c98769
VitJRBOG <vitjrbog@icloud.com>
2020-12-18 20:17:26 +0500

    Doc - добавлены описания публичных функций в пакете chars.

--- chars/chars.go
+++ chars/chars.go
@@ -5,6 +5,7 @@ import "strings"
 var cyrChars = []string{"у", "е", "Е", "х", "Х", "о", "О", "р", "Р", "а", "А", "с", "С"}
 var latChars = []string{"y", "e", "E", "x", "X", "o", "O", "p", "P", "a", "A", "c", "C"}
 
+// CyrToLatForAll заменяет кириллические символы на латинские во всех словах
 func CyrToLatForAll(origText string) string {
 	changedText := origText
 	for i, cyrChar := range cyrChars {
@@ -14,6 +15,7 @@ func CyrToLatForAll(origText string) string {
 	return changedText
 }
 
+// CyrToLatForKeywords заменяет кириллические символы на латинские в словах из списка
 func CyrToLatForKeywords(origText string, keywords []string) string {
 	changedText := origText
 
@@ -32,6 +34,7 @@ func CyrToLatForKeywords(origText string, keywords []string) string {
 	return changedText
 }
 
+// LatToCyr заменяет латинские символы на кириллические во всех словах
 func LatToCyr(origText string) string {
 	changedText := origText
 	for i, latChar := range latChars {
